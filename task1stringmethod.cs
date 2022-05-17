5b55ed52e7f731c07d08d2d4dacf461cd8ea98a5
Your Name <you@example.com>
2022-05-17 18:28:43 +0400

    added newtask

---
+++ task1stringmethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string soz = "Nurlan Faiq Yusif";
+           
+            
+            char[] array;
+            array = soz.ToCharArray();
+            
+
+            {
+                for (int i =array.Length-1; i>=0; i--)
+                { 
+                    Console.WriteLine(array[i]); ;
+
+                }
+
+            }
+        }
+    }
+}
